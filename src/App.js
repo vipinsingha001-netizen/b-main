cd69e16668b43f3faf9e5666605e6df86a9c54ea
vipinsingha001-netizen <vipinsingha001@gmail.com>
2026-02-09 13:41:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SixaMobile from './Comp/SixaMobile';
 import SixaMobileApp from './Comp/SixaApp';
